27b7b32bd6a3ffa52dc4db2f75673a96f50bd8a0
SammuelBlebo <samuel.blebo@yahoo.com>
2023-03-21 15:53:13 +0000

    Tool Menu Icons Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 
+import { ToolsMenu } from "./components/ToolsMenu";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <ToolsMenu />
     </div>
   );
 }
